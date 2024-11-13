fdbcf75f5a0a5ce29b1a4f80479d5648a5223a24
CWilloughby98 <carloswl1998@gmail.com>
2024-11-14 00:40:44 +0100

    initial commit/ added login sw-doc

--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-const router: Router = express.Router()
+const router: Router = express.Router();
 
 /**
  *  @swagger
---
+++ src/routes/login.ts
@@ -0,0 +1,57 @@
+import express, { Router } from 'express';
+
+const router: Router = express.Router();
+
+const SECRET_KEY = 'your_secret_key';
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login with email
+ *     tags: [{User}]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 format: email
+ *                 example: "12093sjiad0109dsao"
+ *               email:
+ *                 type: string
+ *                 example: "sample@mail.com"
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "pass1234"
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "token-string"
+ *       400:
+ *         description: Bad Request || Invalid request parameters
+ *       401:
+ *         description: Unauthorized || Invalid credentials
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
+
+
+router.post("/", (req, res) => {
+    
+})
+
+export default router
