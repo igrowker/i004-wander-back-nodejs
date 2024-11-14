375eb69d308ba12e4c2446b0d3cc4d83e75946ab
CWilloughby98 <carloswl1998@gmail.com>
2024-11-14 13:54:26 +0100

    created login layout

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import axios from 'axios';
 
 const router: Router = express.Router();
 
@@ -6,10 +7,10 @@ const SECRET_KEY = 'your_secret_key';
 
 /**
  * @swagger
- * /login:
+ * /auth/login:
  *   post:
- *     summary: User login with email
- *     tags: [User]
+ *     summary: Authenticate user and retrieve JWT token
+ *     tags: [UserAuth]
  *     requestBody:
  *       required: true
  *       content:
@@ -17,10 +18,6 @@ const SECRET_KEY = 'your_secret_key';
  *           schema:
  *             type: object
  *             properties:
- *               id:
- *                 type: string
- *                 format: email
- *                 example: "12093sjiad0109dsao"
  *               email:
  *                 type: string
  *                 example: "sample@mail.com"
@@ -36,9 +33,15 @@ const SECRET_KEY = 'your_secret_key';
  *             schema:
  *               type: object
  *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Success"
  *                 token:
  *                   type: string
  *                   example: "token-string"
+ *                 user:
+ *                   type: object
+ *                   example: {username: "Pedrito", email: "pedret@gmail.com"}
  *       400:
  *         description: Bad Request || Invalid request parameters
  *       401:
@@ -49,9 +52,46 @@ const SECRET_KEY = 'your_secret_key';
  *         description: Internal Server Error
  */
 
+const JAVA_BACKEND_URL = process.env.JAVA_BACKEND_URL;
 
-router.post("/", (req, res) => {
-    
-})
+router.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
 
-export default router
+    if (!email || !password) {
+        return res.status(404).json({message: "Email and password are required."})
+    }
+
+    //Maybe add email format validation
+
+    try {
+        //Send request to the backend to retrieve the jwt
+        const response = await axios.post(`${JAVA_BACKEND_URL}/users/login`, {
+            email,
+            password
+        });
+        //If successful, store the response
+        const { token, user } = response.data
+
+        //Send the token and and user details
+        res.json({
+            message: "Log in successful",
+            token,
+            user
+        })
+    } catch (error: any) {
+        console.log("Error logging in.", error);
+
+        if (error.response) {
+            switch (error.response.status) {
+                case 404:
+                    return res.status(404).json({ message: "User not found" });
+                case 401:
+                    return res.status(401).json({ message: "Invalid credentials" });
+                default:
+                    return res.status(500).json({ message: "Internal server error." });
+            }
+        }
+    }
+});
+
+export default router;
