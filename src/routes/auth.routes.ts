189a28e25cdd280f23483b139b27580d6a7ac1bc
Lichatti <lisandropiatti@gmail.com>
2024-11-18 17:47:06 +0100

    create /auth/register endpoint, add yup library, create swagger documentation register endpoint

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { signup, login, verify, logout } from "../controllers/auth.controllers";
+import { signup, login, verify, logout, register } from "../controllers/auth.controllers";
 import { isAuthenticated } from "../middlewares/verifyToken.middleware";
+
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/verify", isAuthenticated, verify);
 router.post("/logout", logout);
+router.post("/signup", register);
 
 export default router;
