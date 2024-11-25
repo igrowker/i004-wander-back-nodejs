ccab1b0bd860081fb063130003f4e414444c59d7
Lichatti <lisandropiatti@gmail.com>
2024-11-25 16:24:59 +0100

    adding isAutenticated function to routes

--- src/controllers/bookings.controllers.ts
+++ src/controllers/bookings.controllers.ts
@@ -7,12 +7,8 @@ const getBookingsById = async (req: Request, res: Response) => {
     const { id } = req.params;
     const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) {
-      return res.status(401).json({ message: "No token provided" });
-    }
-
     try {
-        const response = await axios.get(`${JAVA_BACKEND_URL}/bookings`, {
+        const response = await axios.get(`${JAVA_BACKEND_URL}/bookings/${id}`, {
             headers: { Authorization: token },
         });
         res.json(response.data);
