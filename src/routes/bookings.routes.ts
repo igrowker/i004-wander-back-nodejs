1b9d70a3e2e7d38805886ffc09b1c3b8e6673d51
CWilloughby98 <carloswl1998@gmail.com>
2024-12-04 19:00:35 +0100

    added isauth to create booking

--- src/routes/bookings.routes.ts
+++ src/routes/bookings.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { getBookingsById, makeBookings, updateBooking, getBookingsByUser, getBookingsByExperience } from "../controllers/bookings.controllers";
+import { isAuthenticated } from "@/middlewares/verifyToken.middleware";
 
 router.get('/:id', getBookingsById);
-router.post('/create', makeBookings);
+router.post('/create', isAuthenticated, makeBookings);
 router.put('/:id', updateBooking);
 router.get('/user/:userId', getBookingsByUser);
 router.get('/experience/:experienceId', getBookingsByExperience);
